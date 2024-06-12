ae271454bb932f4e9b28e20a9e4cecc67e1d309e
northernium <northernium@gmail.com>
2024-06-12 09:28:46 +0700

    Update V2.5

--- calculator.py
+++ calculator.py
@@ -1,75 +1,79 @@
 '''
 Calculator Module
 '''
-print('Welcome to the python calculator V.2')
-print('Now this calculator has been upgraded to be able to operate with more than 2 numbers')
+print('Welcome to the python calculator V.2.5')
 print('Just type "done" when you finish to see the result')
 
 def add(f,s):
     '''Add'''
     try:
-        return int(f) + int(s)
+        return float(f) + float(s)
     except ValueError:
         return 'Invalid Number'
 
-def substrac(f,s):
-    '''Substrac'''
+def substract(f,s):
+    '''Substract'''
     try:
-        return int(f) - int (s)
+        return float(f) - float (s)
     except ValueError:
         return 'Invalid Number'
 
 def devide(f,s):
     '''Devide'''
+    if s == 0:
+        return "Error divide by 0"
     try:
-        return int(f) / int (s)
+        return float(f) / float (s)
     except ValueError:
         return 'Invalid Number'
 
 def multiple(f,s):
     '''Multiplication'''
     try:
-        return int(f) * int (s)
+        return float(f) * float (s)
     except ValueError:
         return 'Invalid Number'
 
-def result(f,s):
-    '''Calculation'''
-    if opr == '+':
-        return add(f, s)
-    elif opr == '-':
-        return substrac(f,s)
-    elif opr == '*':
-        return multiple(f, s)
-    elif opr == '/':
-        return devide(f, s)
-    else:
-        return None
-
+opr = None
+tot = 0
+num = 0
+calc = 0
 while True: #Added while loop so user can input more than just one number
-    num1 = input('input number: ')
-    if num1 == 'done':
-        break
-    try:
-        x = result(x, num1)
-    except NameError:
-        None
+    if calc == 0:
+        num = input('input number: ') ##Added total calculation for clarity
+    else:
+        num = input(f'{tot} {opr} ')
+        if num == 'done':
+            if calc == 0:
+                print("no numbers entered")
+                break
 
-    opr = input('input operation: ')
+    try:
+        num = float(num)
+        calc = calc + 1
+        if tot == 0:
+            tot = num
+        else:
+            if opr == '+':
+                tot = add(tot, num)
+            elif opr == '-':
+                tot = substract(tot,num)
+            elif opr == '*':
+                tot = multiple(tot, num)
+            elif opr == '/':
+                tot = devide(tot, num)
+    except ValueError:
+        print('invalid input')
+
     if opr == 'done':
         break
 
-    num2 = input('input number: ')
-    if num2 == 'done':
-        break
+    if calc == 1:
+        opr = input('input operation: ')
+    elif calc > 1:
+        opr = input(f'{tot} ')
+        if opr == 'done':
+            break
 
-    try:
-        x = result(x, num2)
-    except NameError:
-        x = result(num1, num2)
-
-    opr = input('input operation: ')
-    if opr == 'done':
-        break
-
-print ('result :' , x)
+print ('result :' , tot)
+print ('total calculation :' , calc)
